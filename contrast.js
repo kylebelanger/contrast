0278a74df475c835e3b3325afd5f601659319249
Kyle Belanger <belanger.kyle@gmail.com>
2016-06-17 13:52:40 -0400

    Add extension components

---
+++ contrast.js
@@ -0,0 +1,15 @@
+
+/*  Contrast
+ *  Google Chrome accessibility extension to invert webpage colors, and reduce contrast ranges.
+ *
+ *  Origin: June 16, 2016
+ *  License: MIT
+*/
+
+/*  eventListener
+ *  Attach an event listener to document on load
+ *
+*/
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Loaded...");
+});
